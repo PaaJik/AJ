c374fbfe0df9dd5b7cd8e2212ba7506463deed85
PaaJik <56014962+PaaJik@users.noreply.github.com>
2019-11-19 16:15:10 +0900

    Update *pthread_join.c

--- *pthread_join.c
+++ *pthread_join.c
@@ -10,4 +10,23 @@ int main() {
     int a = 4; //첫번째 스레드의 인자
     int b = 5;
     int c = 6;
+    
+   status[0] = pthread_create(&thread_id[0], NULL, do_sum, (void *)&a);
+   status[1] = pthread_create(&thread_id[1], NULL, do_sum, (void *)&a);
+   status[2] = pthread_create(&thread_id[2], NULL, do_sum, (void *)&a);
+   pthread_join(&thread_id[0], (void **)&status[0]); 
+   pthread_join(&thread_id[1], (void **)&status[1]); 
+   pthread_join(&thread_id[2], (void **)&status[2]); 
+   
+   printf("Main thread END \n");
+}
+
+void *do_sum(void *arg){
+    int max = *((int *)arg);
+    int sum = 0;
+    for(int i=0;i<=max;i++){
+        sum += i;
+        printf("%d - ADD %d\n",max,i);
+        }
+    printf("1부터 %d까지의 합은 %d입니다\n",max,sum);
 }
